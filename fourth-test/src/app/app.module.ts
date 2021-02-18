26e63de85e49a7ce284984c0f06b6c698365c069
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-02-18 13:41:54 +0530

    fourth test

--- fourth-test/src/app/app.module.ts
+++ fourth-test/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
